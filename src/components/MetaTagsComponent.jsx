c69d2267750ed0bd61338d4b93b112f4bd2c653b
ShivnandanJha <neureto12@gmail.com>
2025-08-02 11:16:24 +0530

    dgbv

--- src/components/MetaTagsComponent.jsx
+++ src/components/MetaTagsComponent.jsx
@@ -4,6 +4,11 @@ import { Helmet } from 'react-helmet';
 const MetaTagsComponent = () => (
   <Helmet>
     <link rel = "canonical" href = "https://www.paramounthospital.in" />
+    <title>Best Multispeciality Hospital in Siliguri | Paramount Hospital</title>
+  <meta
+    name="description"
+    content="Paramount Hospital, Siliguri: Trusted multispeciality hospital with expert doctors near you, 24/7 emergency care, and advanced medical treatments."
+  />
 
   </Helmet>
 );
